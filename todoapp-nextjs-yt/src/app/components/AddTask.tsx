17c0a655cc28b5556a542b13a9768b19415aaaca
ogasawarayui <ogasawara.yui@outlook.com>
2024-03-24 18:19:10 +0900

    app

--- todoapp-nextjs-yt/src/app/components/AddTask.tsx
+++ todoapp-nextjs-yt/src/app/components/AddTask.tsx
@@ -6,7 +6,7 @@ const AddTask = () => {
       <input
         type="text"
         className="w-full border px-4 py-2 rounded-1g focus:outline-none focus:border-blue-400" />
-      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95">
+      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200">
         AddTask
       </button>
     </form>
--- todoapp-nextjs-yt/src/app/components/TodoList.tsx
+++ todoapp-nextjs-yt/src/app/components/TodoList.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <ul className="space-y-3">
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span>散歩</span>
+        <div>
+          <button>edit</button>
+          <button>Delete</button>
+        </div>
+      </li>
+      TodoList
+    </ul>
+  );
 };
 
 export default TodoList;
