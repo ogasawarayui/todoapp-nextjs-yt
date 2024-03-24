662fca9d2acf733093a5533ac10bd15bcfed173a
ogasawarayui <ogasawara.yui@outlook.com>
2024-03-24 17:55:04 +0900

    app

--- todoapp-nextjs-yt/src/app/page.tsx
+++ todoapp-nextjs-yt/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Nextjs 13 Todo App</h1>
-      <div>
-        <div>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4x1 font-bold text-gray-700 -mt-32">
+        Nextjs 13 Todo App
+      </h1>
+      <div className="w-full max-w-x1 mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-1g">
         </AddTask>
       </TodoList>
     </div>
