f7c3a2bbb2917317410f077aa6ce8b103d93b384
ogasawarayui <ogasawara.yui@outlook.com>
2024-03-24 17:38:27 +0900

    app

--- todoapp-nextjs-yt/src/app/page.tsx
+++ todoapp-nextjs-yt/src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <h1>Nextjs 13 Todo App</h1>
+      <div>
+        <div>
+        </AddTask>
+      </TodoList>
+    </div>
+    </div >
+  </main >
+  );
 }
