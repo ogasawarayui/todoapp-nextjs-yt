b52bb949a42d83573df05ee7fb0048304efad8d7
ogasawarayui <ogasawara.yui@outlook.com>
2024-03-24 20:15:41 +0900

    app

--- todoapp-nextjs-yt/src/app/components/Todo.tsx
+++ todoapp-nextjs-yt/src/app/components/Todo.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <li
+    key={todo.id}
+    className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+    <span>{todo.text}</span>
+    <div>
+      <button className="text-green-500 mr-3">edit</button>
+      <button className="text-red-500">Delete</button>
+    </div>
+  </li>
+  );
 };
 
 export default Todo;
--- todoapp-nextjs-yt/src/app/components/TodoList.tsx
+++ todoapp-nextjs-yt/src/app/components/TodoList.tsx
@@ -1,25 +1,16 @@
-import { Todo } from "@/types"
+import { Task } from "@/types"
 import React, { useEffect } from "react";
-
+import Todo from "./Todo";
 interface TodoListProps {
-  todos: Todo[];
+  todos: Task[];
 }
 
 const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className="space-y-3">
       {todos.map((todo) => (
-        <li
-          key={todo.id}
-          className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-          <span>{todo.text}</span>
-          <div>
-            <button className="text-green-500 mr-3">edit</button>
-            <button className="text-red-500">Delete</button>
-          </div>
-        </li>
+        <Todo key={todo.id} todo={todo} />
       ))}
-      TodoList
     </ul>
   );
 };
